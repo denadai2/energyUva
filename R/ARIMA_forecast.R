e4d1ddbb4fd2b51176e5ca7c3dace7b120603460
Marco De Nadai <me@marcodena.it>
2014-09-07 22:57:37 +0200

    R scripts updated

--- R/ARIMA_forecast.R
+++ R/ARIMA_forecast.R
@@ -3,9 +3,9 @@ library(xts)
 library(forecast)
 Sys.setenv(TZ = "GMT")
 fmt <- '%Y-%m-%d %H:%M:%S'
-dat <- read.zoo('740-NTH_elektriciteit_withWeather_clean.csv', tz='GMT', format=fmt, header=TRUE, sep=',', stringsAsFactors=FALSE, index.column=9)
+dat <- read.zoo('../datasets/740-NTH_elektriciteit_withWeather_clean.csv', tz='GMT', format=fmt, header=TRUE, sep=',', stringsAsFactors=FALSE, index.column=9)
 dat.xts <- as.xts(dat$gas..m3.)
-
+dat.xts <- dat.xts["/2014-03-31 00:00"]
 
 # Predicted is the forecast variable, predictedHi is the highest 95% confidence forecast
 predicted <- vector()
@@ -16,7 +16,7 @@ dat.ts <- ts(drop(coredata(dat.xts)), frequency=24, start=1)
 print(floor(length(dat.xts)/24))
 for(i in 0:(floor(length(dat.xts)/24)-21)){ #
 	tempTS <- window(dat.ts, start=(i+1), end=(21+i), frequency=24)
-	if (i%%20 == 0){
+	if (i%%10 == 0){
 			arima <- auto.arima(tempTS)
 		} else {
 			arima <- Arima(tempTS, model=arima)
@@ -30,13 +30,13 @@ for(i in 0:(floor(length(dat.xts)/24)-21)){ #
 	actual <- append(actual, sliced)
 }
 # It forecasts one hour more than necessary
-predicted <- predicted[-length(predicted)]
-predictedHi <- predictedHi[-length(predictedHi)]
+#predicted <- predicted[-length(predicted)]
+#predictedHi <- predictedHi[-length(predictedHi)]
 
 export <- xts(predicted, index(dat.xts["2008-01-21 01:00/"]))
 colnames(export)[NCOL(export)] <- "predicted"
 export$predictedHi <- xts(predictedHi, index(dat.xts["2008-01-21 01:00/"]))
-write.zoo(export, "ARIMA_martedi.csv", sep=",")
+write.zoo(export, "../datasets/ARIMA_forecast.csv", sep=",")
 
 
 
--- R/residuals.R
+++ R/residuals.R
@@ -3,28 +3,38 @@ library(xts)
 library(forecast)
 Sys.setenv(TZ = "GMT")
 fmt <- '%Y-%m-%d %H:%M:%S'
-dat <- read.zoo('740-NTH_elektriciteit_withWeather_clean.csv', tz='GMT', format=fmt, header=TRUE, sep=',', stringsAsFactors=FALSE, index.column=9)
+dat <- read.zoo('../datasets/740-NTH_elektriciteit_withWeather_clean.csv', tz='GMT', format=fmt, header=TRUE, sep=',', stringsAsFactors=FALSE, index.column=9)
 dat.xts <- as.xts(dat$gas..m3.)
+dat.xts <- dat.xts["/2014-03-31 00:00"]
 
 dat.ts <- ts(drop(coredata(dat.xts)), frequency=24, start=1)
 fit <- stl(dat.ts, "per")
 export <- xts(fit$time.series[,"remainder"], index(dat.xts))
 colnames(export)[NCOL(export)] <- "remainder"
 export$trend <- xts(fit$time.series[,"trend"], index(dat.xts))
-write.zoo(export, "dailyResiduals.csv", sep=",")
+write.zoo(export, "../datasets/residuals_day.csv", sep=",")
 
 dat.ts <- ts(drop(coredata(dat.xts)), frequency=(7*24), start=1)
 fit <- stl(dat.ts, "per")
 export <- xts(fit$time.series[,"remainder"], index(dat.xts))
 colnames(export)[NCOL(export)] <- "remainder"
 export$trend <- xts(fit$time.series[,"trend"], index(dat.xts))
-write.zoo(export, "weeklyResiduals.csv", sep=",")
+write.zoo(export, "../datasets/residuals_week.csv", sep=",")
 
 dat.ts <- ts(drop(coredata(dat.xts)), frequency=(365*24), start=1)
 fit <- stl(dat.ts, "per")
 export <- xts(fit$time.series[,"remainder"], index(dat.xts))
 colnames(export)[NCOL(export)] <- "remainder"
 export$trend <- xts(fit$time.series[,"trend"], index(dat.xts))
-write.zoo(export, "annualResiduals.csv", sep=",")
+write.zoo(export, "../datasets/residuals_year.csv", sep=",")
+
+# electricity
+dat.xts <- as.xts(dat$elektriciteit..kwh.)
+dat.ts <- ts(drop(coredata(dat.xts)), frequency=24, start=1)
+fit <- stl(dat.ts, "per")
+export <- xts(fit$time.series[,"remainder"], index(dat.xts))
+colnames(export)[NCOL(export)] <- "remainder"
+export$trend <- xts(fit$time.series[,"trend"], index(dat.xts))
+write.zoo(export, "../datasets/residuals_electricity_day.csv", sep=",")
 
 
